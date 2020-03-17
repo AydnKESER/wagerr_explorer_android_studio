ac35a9365d7521fccb4ff69b1bf71574605ae1c4
jesus <aydnkeser22@gmail.com>
2020-03-18 00:10:46 +0300

    json crash

--- MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.renderscript.Sampler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,14 +28,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity  {
 
+    private Handler mHandler = new Handler();
 
     private BottomNavigationView mMainNav;
     private FrameLayout mFrame;
-
+    JSONObject events;
     private fragmentexplorer fragmentexplorer;
     private fragmentevents fragmentevents;
     private tracerfragment tracerfragment;
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity  {
 
     String wagerurl = "https://explorer.wagerr.com/api/coin/";
     RequestQueue queue;
-
+    JSONObject resp;
 
 
     @Override
@@ -52,10 +52,15 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+//in your OnCreate() method
+
         queue= NetworkController.getInstance(this).getRequestQueue();
+       // queue.add(new JsonObjectRequest(0, wagerurl,null, new listener(),new error()));
+
+
         queue.add(new JsonObjectRequest(0, wagerurl,null, new listener(),new error()));
-
-
 
         mFrame = (FrameLayout) findViewById(R.id.main_frame);
         mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
@@ -65,8 +70,7 @@ public class MainActivity extends AppCompatActivity  {
         tracerfragment= new tracerfragment();
         settingsfragment = new settingsfragment();
 
-
-        getWindow().setNavigationBarColor(getResources().getColor(R.color.navigationBarColor));
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.navigationBarColor)); // alt bar siyah
 
         mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -74,6 +78,7 @@ public class MainActivity extends AppCompatActivity  {
 
                 switch (menuItem.getItemId()){
                     case R.id.nav_explorer : SetFragment(fragmentexplorer);
+
                     return true;
                     case R.id.nav_events : SetFragment(fragmentevents);
                     return true;
@@ -88,7 +93,7 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
 
-
+        mToastRunnable.run();
         SetFragment(fragmentexplorer);
     }
 
@@ -107,6 +112,14 @@ public class MainActivity extends AppCompatActivity  {
     private class listener implements Response.Listener<JSONObject>{
         public void onResponse(JSONObject response){
             Log.i("mesaj","Gelen"+response.toString());
+            try {
+                resp = response.getJSONObject("capEur");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.i("mesaj","Gelen"+resp.toString());
+            //events=response;
+
         }
 
     }
@@ -115,4 +128,15 @@ public class MainActivity extends AppCompatActivity  {
             Toast.makeText(MainActivity.this,"text :"+error.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
+    private Runnable mToastRunnable = new Runnable() {
+        public void run() {
+           // Toast.makeText(MainActivity.this, "This is a delayed toast", Toast.LENGTH_SHORT).show();
+            queue.add(new JsonObjectRequest(0, wagerurl,null, new listener(),new error()));
+            mHandler.postDelayed(this, 15000);
+
+        }
+    };
 }
--- MyApplication7/app/src/main/java/com/example/myapplication/fragmentexplorer.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/fragmentexplorer.java
@@ -17,17 +17,25 @@ import android.widget.TextView;
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class fragmentexplorer extends Fragment {
 
+
     public fragmentexplorer() {
-        // Required empty public constructor
+
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_fragmentexplorer, container, false);
+
+
+
     }
 }
